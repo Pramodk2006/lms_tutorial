b08c19ad2a226e14b2a09f033a0280ebfa0f199d
Pramodk2006 <pramod3132006@gmail.com>
2026-01-30 16:53:32 +0530

    auth page in progress 2

--- app/(auth)/(routes)/signin/page.tsx
+++
@@ -1,5 +0,0 @@
-const page = () => {
-    return ( "this is a protected page" );
-}
- 
-export default page;
--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -3,15 +3,12 @@
 import Image from "next/image";
 import React from "react";
 import { Calendar } from "@/components/ui/calendar";
-
+import { UserButton } from "@clerk/nextjs";
 export default function Home() {
-  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
   return (
     <div>
-      <p> this is a protected page
-        
-      </p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }

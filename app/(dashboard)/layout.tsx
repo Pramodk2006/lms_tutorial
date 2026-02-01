c945a0e1422d29bb276c93f410cef1794637a329
Pramodk2006 <pramod3132006@gmail.com>
2026-02-01 22:47:43 +0530

    style in progress

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ const dashboardLayout = ({
     children
 }:{children:React.ReactNode}) => {
     return (<div className="h-full">
-        <div className="flex h-full w-56 flex-col inset-y-0">
+        <div className="flex h-full w-56 flex-col inset-y-0 hover:bg-gray-800 fixed">
         <SideBar/>
         </div>
         {children}

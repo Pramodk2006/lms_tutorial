48e061fd0f82ec48879c2be801ba2dbc1aabf0ef
Pramodk2006 <pramod3132006@gmail.com>
2026-02-02 23:20:20 +0530

    style appended

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,7 +2,7 @@ const AuthLayout  = ({ children }:{
     children: React.ReactNode
 }) => {
     return (
-        <div className="h-full flex items-center justify-center">
+        <div className="h-full flex items-center justify-center bg-gray-100 p-4">
             {children}
         </div>
       );

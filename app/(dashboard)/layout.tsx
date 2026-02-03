bee3f43c317720aa39be25f65a2f3d09f3463a8e
Pramodk2006 <pramod3132006@gmail.com>
2026-02-03 23:32:16 +0530

    style added

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -4,7 +4,7 @@ const dashboardLayout = ({
     children
 }:{children:React.ReactNode}) => {
     return (<div className="h-full">
-        <div className="flex h-full w-56 flex-col flex-10 inset-y-0 hover:bg-gray-800 fixed">
+        <div className="flex h-full w-56 flex-col inset-y-0 hover:bg-gray-800 fixed">
         <SideBar/>
         </div>
         {children}
